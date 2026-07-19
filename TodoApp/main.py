106dd5298c32276be8da0e478a3e245239017caf
Venkatesh <venkateshmahanteshkaladagi.241me163@nitk.edu.in>
2026-07-19 17:22:04 +0530

    Starting with API Request Methods section

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -14,3 +14,10 @@ app=FastAPI()
 
 Models.Base.metadata.create_all (bind=engine) # Now if we start the server we will successfully create our database
                                               # Now we will install Sqlite to play around this db using query commands
+'''
+    Enter 'sqlite3 todos.db' in terminal to activate sqlite3
+    # .schema :- Shows all the tables in our db currently
+    # adding Todo:- insert into todos (title,description,priority,complete) values ('Go to the store','Pick up eggs', 5, False);
+    # Viewing the entries:- select * from todos; :- We can do .mode to change the viewing of the tables. Ex:- .mode markdown, .mode column etc
+    # Delete the todo:- delete * from todos where id=4
+'''
